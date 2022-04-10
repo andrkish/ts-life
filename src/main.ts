0e8a10510fea262bd65d8c83269187fe5633b219
Andrii Kishchenko <andrii.kishchenko@itechhub.io>
2022-04-10 19:02:41 +0300

    refactoring: create game state as separate function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { Render, Game, State, GameOptions } from './engine';
 import { setup } from './setup';
 
+function create32x32(canvas:HTMLCanvasElement,
+  context:CanvasRenderingContext2D,
+  img:HTMLImageElement) {
+  const state = new State(19, 25);
+  const spriteSize = { width: 32, height: 32 };
+  const fieldSize = { width: 32, height: 32 };
+  const render = new Render(canvas, context, img, fieldSize, spriteSize);
+
+  const options = new GameOptions(false, 1);
+
+  new Game(state, render, options);
+}
+
 function initApp() {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const context = canvas.getContext('2d');
@@ -8,14 +21,7 @@ function initApp() {
   const img = new Image();
   img.onload = function() {
     setup();
-    const state = new State(19, 25);
-    const spriteSize = { width: 32, height: 32 };
-    const fieldSize = { width: 32, height: 32 };
-    const render = new Render(canvas, context, img, fieldSize, spriteSize);
-
-    const options = new GameOptions(false, 1);
-
-    new Game(state, render, options);
+    create32x32(canvas, context, img);
   };
   img.src = 'spritesheet.png';
 }
